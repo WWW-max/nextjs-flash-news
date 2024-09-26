46b474fd824477040b07a1a9361f78b2af25c86d
www-max <464711201@qq.com>
2024-09-26 12:25:09 +0800

    test: depoly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
+const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: 'export', // 使用静态导出
+  basePath: isProd ? '/nextjs-flash-news' : '',
+  images: {
+    unoptimized: true, // 禁用图片优化
+  },
+};
 
 export default nextConfig;
