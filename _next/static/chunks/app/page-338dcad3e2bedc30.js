5c8071235fdf3f63fc023af9751a8b2e94d19b90
WWW-max <WWW-max@users.noreply.github.com>
2024-09-26 10:42:07 +0000

    deploy: ca4f59fabac45e07db03493341e853315d508fd6

--- _next/static/chunks/app/page-338dcad3e2bedc30.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{698:function(n,e,u){Promise.resolve().then(u.t.bind(u,6323,23))}},function(n){n.O(0,[323,111,487,744],function(){return n(n.s=698)}),_N_E=n.O()}]);
