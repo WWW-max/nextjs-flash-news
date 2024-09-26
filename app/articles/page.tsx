c2eb756452f28bdee4637375a7b3cd250a1607b8
www-max <464711201@qq.com>
2024-09-26 12:50:44 +0800

    test: image2

--- app/articles/page.tsx
+++ app/articles/page.tsx
@@ -3,6 +3,6 @@ import Image from "next/image";
 export default function ArticlesPage(){
     return <div>
         Hi from ArticlePage
-        <Image src='/favicon.ico' width={45} height={45} alt="img" />
+        <Image src='nextjs-flash-news/favicon.ico' width={45} height={45} alt="img" />
     </div>
 }
