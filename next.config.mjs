0a458d99e40094e01977e3677aa207e06157578f
www-max <464711201@qq.com>
2024-09-26 16:08:17 +0800

    chore: build

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,8 @@
 const nextConfig = {
     output: 'export',
 //   basePath: '',
+    assetPrefix: process.env.ASSET_PREFIX,
+    basePath: process.env.BASE_PATH,
   images: {
     unoptimized: true,
   }, 
