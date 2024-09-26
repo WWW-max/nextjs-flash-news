174c2b31257f7e6cc00dc8f442b1f97ee51bf0c7
www-max <464711201@qq.com>
2024-09-26 13:16:04 +0800

    fix: basePath

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export', // 使用静态导出
-  basePath: isProd ? '/nextjs-flash-news' : '',
+  basePath: '/nextjs-flash-news',
   images: {
     unoptimized: true, // 禁用图片优化
   },
